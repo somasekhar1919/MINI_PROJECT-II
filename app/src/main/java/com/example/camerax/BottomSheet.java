1978e025db8d90cece415f82f4a3136c914f64b9
juniorcoder <somashaker23@gmail.com>
2022-10-18 01:07:48 +0530

    scan working and result are getting on screen but in main activity with camera running continuously

---
+++ app/src/main/java/com/example/camerax/BottomSheet.java
@@ -0,0 +1,22 @@
+package com.example.camerax;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class BottomSheet extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view =  inflater.inflate(R.layout.fragment_bottom_sheet, container, false);
+        return view;
+    }
+}
