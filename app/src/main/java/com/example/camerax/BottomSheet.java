1eb7fae4599fb2c4922f25104f193f46bdc172b6
juniorcoder <somashaker23@gmail.com>
2022-10-21 20:45:57 +0530

    Variable home page with Scan Button which triggers the CameraPreview Fragment. Qr scan result shows up in the home page after successful scan with 2 new buttons outing and food.

--- app/src/main/java/com/example/camerax/BottomSheet.java
+++ app/src/main/java/com/example/camerax/BottomSheet.java
@@ -1,22 +1,207 @@
 package com.example.camerax;
 
+import static android.content.ContentValues.TAG;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.media.AudioManager;
+import android.media.Image;
+import android.media.ToneGenerator;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.barcode.common.Barcode;
+import com.google.mlkit.vision.common.InputImage;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
-public class BottomSheet extends Fragment {
+public class BottomSheet extends BottomSheetDialogFragment {
 
-    View view;
+
+    private androidx.camera.view.PreviewView PreviewView;
+    private ListenableFuture<ProcessCameraProvider> CameraProviderFuture;
+    private final ImageAnalysis.Analyzer qrimageanalysis = new QRimageanalysis();
+    ItemViewModel viewmodel;
+
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view =  inflater.inflate(R.layout.fragment_bottom_sheet, container, false);
+        View view = inflater.inflate(R.layout.fragment_bottom_sheet, container, false);
+
+        PreviewView = view.findViewById(R.id.ScanPreview);
+
         return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        ExecutorService cameraService = Executors.newSingleThreadExecutor();
+        CameraProviderFuture = ProcessCameraProvider.getInstance(requireContext());
+        CameraProviderFuture.addListener(() -> {
+            try {
+                if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != (PackageManager.PERMISSION_GRANTED)) {
+                    ActivityCompat.requestPermissions(requireActivity(),new String[]{Manifest.permission.CAMERA}, 101);
+
+                } else {
+                    ProcessCameraProvider Cameraprovider = CameraProviderFuture.get();
+                    startCameraX(Cameraprovider);
+
+                }
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, getExecuter());
+    }
+
+    private Executor getExecuter () {
+            return ContextCompat.getMainExecutor(requireContext());
+        }
+
+
+    @SuppressLint("RestrictedApi")
+    private void startCameraX(ProcessCameraProvider cameraprovider) {
+        cameraprovider.unbindAll();
+
+        //Cameraselecter use case
+        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+
+        //Preview usecase
+        Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(PreviewView.getSurfaceProvider());
+
+
+    //imageAnalysis usecase
+        ImageAnalysis imageanalysis = new ImageAnalysis.Builder()
+                .setTargetResolution(new Size(320,720))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_BLOCK_PRODUCER).build();
+
+        imageanalysis.setAnalyzer(getExecuter(),qrimageanalysis);
+
+
+
+        cameraprovider.bindToLifecycle(this, cameraSelector, preview,imageanalysis);
+
+    }
+
+    public class QRimageanalysis implements ImageAnalysis.Analyzer{
+
+        @Override
+        public void analyze(@NonNull ImageProxy image) {
+
+            ScanImageForBarcodes(image);
+
+
+        }
+
+        private void ScanImageForBarcodes(ImageProxy image) {
+            @SuppressLint("UnsafeOptInUsageError")
+            Image image1 = image.getImage();
+            if(image1 != null) {
+                InputImage inputImage = InputImage.fromMediaImage(image1, image.getImageInfo().getRotationDegrees());
+
+                BarcodeScannerOptions options =
+                        new BarcodeScannerOptions.Builder()
+                                .setBarcodeFormats(
+                                        Barcode.FORMAT_QR_CODE)     // for all formats
+                                .build();
+
+                BarcodeScanner scanner = BarcodeScanning.getClient(options);
+
+                Task<List<Barcode>> result = scanner.process(inputImage);
+                result.addOnSuccessListener(this::readerbarcode)                      // if image scanned smoothly
+                        .addOnFailureListener(e -> Log.d(TAG, "onFailure: scan failed"))
+                        .addOnCompleteListener(task -> image.close());
+            }
+
+        }
+
+        private void readerbarcode(List<Barcode> barcodes) {
+            String testtext = "koooo";
+
+            for (Barcode barcode: barcodes) {
+                Log.d(TAG, "01:loop " );
+
+                Rect bounds = barcode.getBoundingBox();
+                Point[] corners = barcode.getCornerPoints();
+                
+                String[] rawValue ;
+                try {
+
+                    rawValue = barcode.getRawValue().split(",");
+                    Log.d(TAG, "readerbarcode: " + rawValue);
+
+
+
+//                    viewmodel = new ViewModelProvider(requireActivity()).get(ItemViewModel.class);
+//                    viewmodel.setData(testtext);
+
+                    Log.d(TAG, "901: " + rawValue[0]);
+
+                    ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+                    toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150);
+                    TextView Name = getActivity().findViewById(R.id.Name);
+                    TextView RollNumber = getActivity().findViewById(R.id.RollNumber);
+                    TextView Branch = getActivity().findViewById(R.id.Branch);
+                    TextView Degree = getActivity().findViewById(R.id.Degree);
+                    Name.setVisibility(View.VISIBLE);
+                    RollNumber.setVisibility(View.VISIBLE);
+                    Branch.setVisibility(View.VISIBLE);
+                    Degree.setVisibility(View.VISIBLE);
+                    Name.setText(rawValue[0]);
+                    RollNumber.setText(rawValue[3]);
+                    Branch.setText(rawValue[2]);
+                    Degree.setText(rawValue[1]);
+
+                    LinearLayout Action = getActivity().findViewById(R.id.action);
+                    Action.setVisibility(View.VISIBLE);
+                    getActivity().getSupportFragmentManager().beginTransaction().remove(BottomSheet.this).commit();
+                    break;
+
+    }catch (Exception e){
+                    e.printStackTrace();
+                }
+}
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/camerax/MainActivity.java
+++ app/src/main/java/com/example/camerax/MainActivity.java
@@ -1,211 +1,99 @@
 package com.example.camerax;
 
 
-import static android.content.ContentValues.TAG;
-
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
-import android.graphics.Point;
-import android.graphics.Rect;
-import android.media.AudioManager;
-import android.media.Image;
-import android.media.ToneGenerator;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.Size;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ImageAnalysis;
-import androidx.camera.core.ImageProxy;
-import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.tasks.Task;
 import com.google.common.util.concurrent.ListenableFuture;
-import com.google.mlkit.vision.barcode.BarcodeScanner;
-import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
-import com.google.mlkit.vision.barcode.BarcodeScanning;
-import com.google.mlkit.vision.barcode.common.Barcode;
-import com.google.mlkit.vision.common.InputImage;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-public class MainActivity extends AppCompatActivity{
-    private ListenableFuture <ProcessCameraProvider> CameraProviderFuture;
+public class MainActivity extends AppCompatActivity {
+    private ListenableFuture<ProcessCameraProvider> CameraProviderFuture;
     private PreviewView PreviewView;
     private ExecutorService CameraService;
-    private TextView Name,RollNumber,Branch,Degree;
-    private QRimageanalysis qrimageanalysis = new QRimageanalysis();
+    private TextView Name, RollNumber, Branch, Degree;
 
+    private Button scan_button,outing_button,food_button;
+    private ItemViewModel viewmodel;
 
+    public MainActivity(){
+        super(R.layout.activity_main);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Name = findViewById(R.id.Name);
-        RollNumber = findViewById(R.id.RollNumber);
-        Branch = findViewById(R.id.Branch);
-        Degree = findViewById(R.id.Degree);
-
-        PreviewView = findViewById(R.id.ScanPreview);
-        this.getWindow().setFlags(1024,1024);
+        scan_button = findViewById(R.id.scan_button);
+        outing_button = findViewById(R.id.outing_button);
+        food_button = findViewById(R.id.food_button);
 
 
+        scan_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CallBottomsheet(new BottomSheet());
 
-        CameraService = Executors.newSingleThreadExecutor();
-        CameraProviderFuture = ProcessCameraProvider.getInstance(this);
-        CameraProviderFuture.addListener(() ->{
-            try {
-                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)!= (PackageManager.PERMISSION_GRANTED)){
-                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},101);
-
-                }else {
-                ProcessCameraProvider Cameraprovider = CameraProviderFuture.get();
-                startCameraX(Cameraprovider);
-                }
-            } catch (ExecutionException | InterruptedException e) {
-                e.printStackTrace();
             }
-        },getExecuter());
+        });
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReferenceFromUrl("https://camerax-d6467-default-rtdb.firebaseio.com/");
+        DatabaseReference table = myRef.child("Records");
+        String currentDateTimeString = java.text.DateFormat.getDateTimeInstance().format(new Date());
+        outing_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        food_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+
+
     }
 
-    private Executor getExecuter() {
-        return ContextCompat.getMainExecutor(this);
+    private void CallBottomsheet(BottomSheet bottomSheet) {
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .replace(R.id.fragment_container_view,BottomSheet.class, null)
+//                .addToBackStack(null)
+                .commit();
+        scan_button.setVisibility(View.GONE);
+
     }
+
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 101 && grantResults.length > 0) {
-            ProcessCameraProvider Cameraprovider = null;
-            try {
-                Cameraprovider = CameraProviderFuture.get();
-            } catch (ExecutionException | InterruptedException e) {
-                e.printStackTrace();
+    public void onBackPressed() {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container_view);
+        if(fragment!=null) {
+            getSupportFragmentManager().beginTransaction()
+                    .remove(fragment)
+                    .commit();
+            scan_button.setVisibility(View.VISIBLE);
+
+        }else{
+                super.onBackPressed();
+
             }
-            assert Cameraprovider != null;
-            startCameraX(Cameraprovider);
         }
-    }
-
-    @SuppressLint("RestrictedApi")
-    private void startCameraX(ProcessCameraProvider cameraprovider) {
-        cameraprovider.unbindAll();
-
-        //Cameraselecter use case
-        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
-
-        //Preview usecase
-        Preview preview = new Preview.Builder().build();
-        preview.setSurfaceProvider(PreviewView.getSurfaceProvider());
-
-
-//imageAnalysis usecase
-        ImageAnalysis imageanalysis = new ImageAnalysis.Builder()
-                .setTargetResolution(new Size(320,720))
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_BLOCK_PRODUCER).build();
-
-        imageanalysis.setAnalyzer(getExecuter(),qrimageanalysis);
-
-
-
-        cameraprovider.bindToLifecycle(this, cameraSelector, preview,imageanalysis);
-    }
-
-    public class QRimageanalysis implements ImageAnalysis.Analyzer{
-
-        @Override
-        public void analyze(@NonNull ImageProxy image) {
-
-           ScanImageForBarcodes(image);
-
-
-        }
-
-        private void ScanImageForBarcodes(ImageProxy image) {
-            @SuppressLint("UnsafeOptInUsageError")
-            Image image1 = image.getImage();
-            if(image1 != null) {
-                InputImage inputImage = InputImage.fromMediaImage(image1, image.getImageInfo().getRotationDegrees());
-
-                BarcodeScannerOptions options =
-                        new BarcodeScannerOptions.Builder()
-                                .setBarcodeFormats(
-                                        Barcode.FORMAT_QR_CODE)     // for all formats
-                                .build();
-
-                BarcodeScanner scanner = BarcodeScanning.getClient(options);
-
-                Task<List<Barcode>> result = scanner.process(inputImage);
-                    result.addOnSuccessListener(this::readerbarcode)                      // if image scanned smoothly
-                            .addOnFailureListener(e -> Log.d(TAG, "onFailure: scan failed"))
-                            .addOnCompleteListener(task -> image.close());
-            }
-
-        }
-
-        private void readerbarcode(List<Barcode> barcodes) {
-
-            for (Barcode barcode: barcodes) {
-
-                Rect bounds = barcode.getBoundingBox();
-                Point[] corners = barcode.getCornerPoints();
-
-
-                String[] rawValue = new String[0];
-                try {
-                    rawValue = barcode.getRawValue().split(",");
-                    Log.d(TAG, "readerbarcode: " + rawValue);
-
-                    Log.d(TAG, "9010: " + rawValue);
-                    Name.setText(rawValue[0]);
-                    RollNumber.setText(rawValue[3]);
-                    Branch.setText(rawValue[2]);
-                    Degree.setText(rawValue[1]);
-                    ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
-                    toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150);
-                    CallBottomsheet(new BottomSheet());
-
-//                FirebaseDatabase database = FirebaseDatabase.getInstance();
-//                DatabaseReference myRef = database.getReferenceFromUrl("https://camerax-d6467-default-rtdb.firebaseio.com/");
-//                DatabaseReference table = myRef.child("Records");
-//                String currentDateTimeString = java.text.DateFormat.getDateTimeInstance().format(new Date());
-//
-//                table.child(rawValue[3] + "/" + currentDateTimeString).setValue("IN");
-//                try {
-//                    Thread.sleep(1000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-
-            }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
-
-        }
-
-        private void CallBottomsheet(BottomSheet bottomSheet) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.ScanPreview,bottomSheet);
-            fragmentTransaction.commit();
-        }
-        }
-
-    }
-
+}
